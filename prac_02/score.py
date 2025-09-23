18a5f35d0e937c4e3c02fbb972fcdaf524c818f2
jd156083 <juliana.jess@my.jcu.edu.au>
2025-09-23 13:30:30 +1000

    Refactor score converter to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,17 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+def main():
+    score = float(input("Enter score: "))
+    print(determine_grade(score))
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def determine_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    return "Bad"
+
+main()
