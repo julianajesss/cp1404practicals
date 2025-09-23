b27123451bb0a52a7b4cb9221e13be9d2f3c4b4b
jd156083 <juliana.jess@my.jcu.edu.au>
2025-09-23 13:36:35 +1000

    Add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,9 +2,14 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
+
+
 def main():
     score = float(input("Enter score: "))
     print(determine_grade(score))
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score}\n{determine_grade(random_score)}")
 
 def determine_grade(score):
     if score < 0 or score > 100:
