49865efa82faf3743d1456a14a7bcc6fa292ad4b
jd156083 <juliana.jess@my.jcu.edu.au>
2025-09-30 12:57:10 +1000

    Print capitalist_conrad.py to file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,15 +9,18 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.175  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+FILENAME = "stock_price_sim.txt"
 
 number_of_days = 0
 price = INITIAL_PRICE
-print(f"Starting price: ${price:,.2f}")
+out_file = open(FILENAME, 'w')
+print(f"Starting price: ${price:,.2f}", file=out_file)
+
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -34,4 +37,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
     number_of_days += 1
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+out_file.close()
