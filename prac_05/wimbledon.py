a9ca874bcacc6be2ea89f15560a205f581fa2650
jd156083 <juliana.jess@my.jcu.edu.au>
2025-10-14 14:16:35 +1000

    Add doc strings

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,11 +1,28 @@
 """
 Wimbledon
 Estimate: 20 minutes
-Actual:   1.34
+Actual:   43 minutes
 """
 
 
 def main():
+    """Get wimbledon winners from file and print winners of wimbledon"""
+    winners_to_wins, countries = format_winners()
+    print_winners(countries, winners_to_wins)
+
+
+def print_winners(countries, winners_to_wins):
+    """Print winners or wimbledon"""
+    print("Wimbledon Champions:")
+    for winner in winners_to_wins:
+        print(f"{winner} {winners_to_wins[winner]}")
+    print()
+    print(f"These {len(countries)} countries have won Wimbledon:")
+    print(", ".join(countries))
+
+
+def format_winners():
+    """Format winners of wimbledon"""
     winners_to_wins = {}
     countries = []
     with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
@@ -24,11 +41,7 @@ def main():
             if winner_data[0] not in countries:
                 countries.append(winner_data[0])
             countries.sort()
-        print("Wimbledon Champions:")
-        for winner in winners_to_wins:
-            print(f"{winner} {winners_to_wins[winner]}")
-        print()
-        print(f"These {len(countries)} countries have won Wimbledon:")
-        print(", ".join(countries))
+    return winners_to_wins, countries
+
 
 main()
