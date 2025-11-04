e4e9b535310d9483c44fcdc1acd81e999c0b539e
julianajesss <juliana.jess@my.jcu.edu.au>
2025-11-04 15:34:00 +1000

    Add init and str for Project class

---
+++ prac_07/project.py
@@ -0,0 +1,19 @@
+class Project:
+    def __init__(self, name="", start_date="", priority=0, cost_estimate=0.0, completion=0):
+        """Represents a project.
+        name: string, name of the project
+        start_date: string, date the project started
+        priority: integer, the priority of the project
+        cost_estimate: float, the estimated cost for the project
+        completion: integer, the percentage completion of the project
+        """
+
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion = completion
+
+    def __str__(self):
+        """Return a string of project information."""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: {self.cost_estimate}, completetion: {self.completion}%"
