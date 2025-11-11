45a896639600f1813cdde85711b704c4bf5fc57a
jd156083 <juliana.jess@my.jcu.edu.au>
2025-11-11 13:50:49 +1000

    Add handle convert

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,15 +1,24 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 
 class ConvertMilesToKmApp(App):
     """ConvertMilesToKmApp is a Kivy app for converting miles to km"""
+    message = StringProperty()
+
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (500, 400)
         self.title = "Convert miles to km"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_convert(self):
+        number = float(self.root.ids.input_field.text)
+        output = number * 1.60934
+        self.message = output
+
 
 ConvertMilesToKmApp().run()
