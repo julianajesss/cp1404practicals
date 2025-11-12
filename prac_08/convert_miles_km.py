80a69fb8143cfe15bdb5f636fa4aad41eec1591d
julianajesss <juliana.jess@my.jcu.edu.au>
2025-11-13 09:09:16 +1000

    Finish converter app

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,10 +3,12 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.properties import StringProperty
 
+MILES_TO_KM = 1.60934
+
 
 class ConvertMilesToKmApp(App):
     """ConvertMilesToKmApp is a Kivy app for converting miles to km"""
-    message = StringProperty()
+    km = StringProperty()
 
     def build(self):
         """ build the Kivy app from the kv file """
@@ -15,10 +17,26 @@ class ConvertMilesToKmApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_convert(self):
-        number = float(self.root.ids.input_field.text)
-        output = number * 1.60934
-        self.message = output
+    def handle_calculate(self, text):
+        """Handle calculation (could be button press or other call)."""
+        miles = self.convert_to_number(text)
+        self.update_result(miles)
+
+    def handle_increment(self, text, change):
+        """Handle up/down button press, update the text input with new value, call calculation function."""
+        miles = self.convert_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
+
+    def update_result(self, miles):
+        self.km = str(miles * MILES_TO_KM)
+
+    @staticmethod
+    def convert_to_number(text):
+        """Convert text to float or 0.0 if invalid."""
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
 
 
 ConvertMilesToKmApp().run()
